4cdf4c3c094be74d7fa2deed20e2d0c433574e0b
quentgar <56115920+quentgar@users.noreply.github.com>
2021-07-20 10:48:11 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 from PIL import Image
 import numpy as np
 
+def crop_perso(img,d=32):
+   """ Make image dim divisible by d """
+   new_size = (img.shape[1] - img.shape[1] % d, 
+                img.shape[2] - img.shape[2] % d)
+
+   img_cropped = img[:,
+                     int((img.shape[1] - new_size[0])/2):int((img.shape[1] + new_size[0])/2),
+                     int((img.shape[2] - new_size[1])/2):int((img.shape[2] + new_size[1])/2)]
+
+   return img_cropped
+
 def format_image(img_path, dim_div_by):
   """ Load image as numpy array and transpose to C x W x H [0..1] """
   img_pil = Image.open(img_path1)
@@ -15,16 +26,17 @@ def format_image(img_path, dim_div_by):
 
   return img_np
 
-def crop_perso(img,d=32):
-   """ Make image dim divisible by d """
-   new_size = (img.shape[1] - img.shape[1] % d, 
-                img.shape[2] - img.shape[2] % d)
+def np_to_torch(img_np):
+    '''Converts image in numpy.array to torch.Tensor.
+    From C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return torch.from_numpy(img_np)[None, :]
 
-   img_cropped = img[:,
-                     int((img.shape[1] - new_size[0])/2):int((img.shape[1] + new_size[0])/2),
-                     int((img.shape[2] - new_size[1])/2):int((img.shape[2] + new_size[1])/2)]
-
-   return img_cropped
+def torch_to_np(img_var):
+    '''Converts an image in torch.Tensor format to np.array.
+    From 1 x C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return img_var.detach().cpu().numpy()[0]
 
 def optimize(parameters, closure, LR, num_iter):
   """ Optimize net with Adam """
