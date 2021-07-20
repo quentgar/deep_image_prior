b0fd2ccc2b6edab9fa669e6c5e814cadaa61361d
quentgar <56115920+quentgar@users.noreply.github.com>
2021-07-20 10:18:04 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,47 @@
 from PIL import Image
+import numpy as np
 
-def format_image(img_path, mask_path, dim_div_by):
-  img_pil, img_np = get_image(img_path, imsize)
-  img_pil = crop_image(img_pil, dim_div_by)
-  img_np = pil_to_np(img_pil)
+def format_image(img_path, dim_div_by):
+  """ Load image as numpy array and transpose to C x W x H [0..1] """
+  img_pil = Image.open(img_path1)
+  ar = np.array(img_pil)
 
-  img_mask_pil, img_mask_np = get_image(mask_path, img_np.shape[1:])
-  img_mask_pil = crop_image(img_mask_pil, dim_div_by)
-  img_mask_np = 1 - pil_to_np(img_mask_pil)
+  if len(ar.shape) == 3:
+    ar = ar.transpose(2,0,1)
 
-  return img_np, img_mask_np
+  img_np = ar.astype(np.float32) / 255.
+
+  img_np = crop_perso(img_np, dim_div_by)
+
+  return img_np
 
 def crop_perso(img,d=32):
+   """ Make image dim divisible by d """
    new_size = (img.shape[1] - img.shape[1] % d, 
                 img.shape[2] - img.shape[2] % d)
 
    img_cropped = img[:,
                      int((img.shape[1] - new_size[0])/2):int((img.shape[1] + new_size[0])/2),
                      int((img.shape[2] - new_size[1])/2):int((img.shape[2] + new_size[1])/2)]
-                     
+
    return img_cropped
+
+def optimize_perso(optimizer_type, parameters1, parameters2, closure, LR_inp, LR_rec, num_iter, ind_iter):
+    print('Starting optimization with ADAM')
+    optimizer_inpainting = torch.optim.Adam(parameters1, lr=LR_inp)
+    optimizer_recalage = torch.optim.Adam(parameters2, lr=LR_rec)
+
+    iter = num_iter // ind_iter
+    
+    for j in range(iter):
+        for i in range(ind_iter):
+          # Optimiser paramètres inpainting
+          optimizer_inpainting.zero_grad()
+          closure()
+          optimizer_inpainting.step()
+
+        for i in range(ind_iter):
+          # Optimiser paramètres recalage
+          optimizer_recalage.zero_grad()
+          closure()
+          optimizer_recalage.step()
