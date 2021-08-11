0cfe85b593b056ea603fa8f3b1b55531d162273f
Nathan-Sob <81183452+Nathan-Sob@users.noreply.github.com>
2021-08-11 11:23:53 +0200

    Update layers.py

--- src/Nathan/layers.py
+++ src/Nathan/layers.py
@@ -174,7 +174,7 @@ def rotate_gconv_kernels(kernel, periodicity=2 * np.pi, diskMask=True):
       # nous : [orientations_nb,channelsOUT,channelsIN,kernelSizeH,kernelSizeW]
       kernels_temp = kernels_planar_rotated[:,:,:,orientation,:,:]
       # [kernelSizeH,kernelSizeW,channelsIN,channelsOUT,orientations_nb]
-      # nous : [channelsOUT,channelsIN,orientations_nb,kernelSizeH,kernelSizeW]
+      # nous : [channelsOUT,channelsIN,kernelSizeH,kernelSizeW,orientations_nb]
       kernels_temp = kernels_temp.permute(1, 2, 3, 4, 0)
       # [kernelSizeH*kernelSizeW*channelsIN*channelsOUT*orientations_nb]
       kernels_temp = torch.reshape(kernels_temp, [kernelSizeH * kernelSizeW * channelsIN * channelsOUT, orientations_nb])
